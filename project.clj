6ab04a183dd79d229e3abba8caa2d1ad359843b6
Francisco Viramontes <kidpollo@gmail.com>
2020-10-30 14:26:59 -0700

    Bump versions for deploy testing

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject kidpollo/lein-npm "0.7.0"
+(defproject kidpollo/lein-npm "0.7.2"
   :description "Manage npm dependencies for CLJS projects"
-  :url "https://github.com/RyanMcG/lein-npm"
+  :url "https://github.com/kidpollo/lein-npm"
   :license {:name "Apache License, version 2.0"
             :url "http://www.apache.org/licenses/LICENSE-2.0.html"}
   :dependencies [[cheshire "5.2.0"]]
